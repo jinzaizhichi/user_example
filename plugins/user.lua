bfb231b217e6fad39b308fc95358ca4dfbcf4859
jinzaizhichi <jinzaizhichi888@gmail.com>
2023-05-24 10:28:51 +0800

    perf codeium

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,14 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+-- Remove the `use` here if you're using folke/lazy.nvim.
+  'Exafunction/codeium.vim',
+   config = function ()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+    vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+    vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+    vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+  end
+
 }
