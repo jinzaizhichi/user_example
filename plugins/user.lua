a1d1aeca86935f3f5b1fdd9fb843db86bc5c13e5
jinzaizhichi <jinzaizhichi888@gmail.com>
2023-06-01 12:54:24 +0800

    add terraform

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,13 +10,18 @@ return {
   --   end,
   -- },
 -- Remove the `use` here if you're using folke/lazy.nvim.
-  'Exafunction/codeium.vim',
-  event = "BufRead",
-  config = function ()
-    -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set('i', '<C-h>', function () return vim.fn['codeium#Accept']() end, { expr = true })
-    vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-    vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-    vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-  end
+  {'Exafunction/codeium.vim',
+      event = "BufRead",
+      config = function ()
+        -- Change '<C-g>' here to any keycode you like.
+        vim.keymap.set('i', '<C-h>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+        vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+        vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+        vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+      end,
+    },
+   {'hashivim/vim-terraform',
+      event = "BufRead",
+    
+    },
 }
